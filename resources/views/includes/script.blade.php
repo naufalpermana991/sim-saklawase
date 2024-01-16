71ff9ae46797b29d3b8742ea30f96ac87d9eaf5c
Muhammad Naufal Permana <53422706+naufalpermana991@users.noreply.github.com>
2024-01-16 14:06:39 +0700

    pembaruan skrip

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -2,7 +2,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
-    <script async
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIN_WUtEVkjgaSir4VeC9YgCc7NF3P9hE&callback=console.debug&libraries=maps,marker&v=beta">
-    </script>
 @endpush
